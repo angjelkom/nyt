ded9c386da3708da793013b0a72853660e1e8d73
Angjelko Miloshevski <angjelkom@surfer-172-30-0-124-hotspot.internet-for-guests.com>
2023-05-31 22:44:04 +0200

    v1.0.3

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
 // index.js
-import { createWriteStream, copyFile } from 'fs'
+import { createWriteStream } from 'fs'
 import { finished } from 'stream/promises'
 import { Readable } from 'stream'
-import im from 'imagemagick'
 
 const main = async () => {
-    let date = new Date();
-    const offset = date.getTimezoneOffset();
-    date = new Date(date.getTime() + (offset * 60 * 1000));
-    let format = date.toISOString().split('T')[0];
+  let date = new Date();
+  const offset = date.getTimezoneOffset();
+  date = new Date(date.getTime() + (offset * 60 * 1000));
+  let format = date.toISOString().split('T')[0];
 
-    const file = await fetch(`https://static01.nyt.com/images/${format.replace(/-/g, '/')}/nytfrontpage/scan.pdf`);
+  const file = await fetch(`https://static01.nyt.com/images/${format.replace(/-/g, '/')}/nytfrontpage/scan.pdf`);
 
-    const fileStream = createWriteStream('/tmp/image.pdf', { flags: 'wx' });
+  const fileStream = createWriteStream('./serve/nytfrontpage.pdf', { flags: 'wx' });
 
-    await finished(Readable.fromWeb(file.body).pipe(fileStream));
-
-    im.convert([
-        '/tmp/image.pdf[0]',
-        '-format', 'jpg',
-        '-resample', '600x600',
-        '-quality', '100',
-        '-strip',
-        '-resize', '1440x2550',
-        `./serve/images/nyt-${format}.jpg`,
-    ], () => copyFile(`./serve/images/nyt-${format}.jpg`, `./serve/images/nytfrontpage.jpg`, () => console.log('Generated New Front Page!')));
+  await finished(Readable.fromWeb(file.body).pipe(fileStream));
 }
 
 
